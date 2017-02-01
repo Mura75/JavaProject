7b861d07ac1429db893b985bf8baf223ab8e2f97
Murager-ПК\Murager <zmurager@gmail.com>
2017-02-01 19:48:43 +0600

    Main function changed.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by Murager on 01.02.2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод текста
+        String input = scanner.nextLine();
+
+
+        // Вывод текста
+        System.out.println(input);
     }
 
 }
