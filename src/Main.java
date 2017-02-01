5a8e20d8aa900aebe53bb29e0c0eda8c63026904
Murager-ПК\Murager <zmurager@gmail.com>
2017-02-01 20:51:19 +0600

    Added example of array.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -16,28 +17,57 @@ public class Main {
         // Vvod teksta s klaviaturi
         String input = scanner.nextLine();
 
-
-
         //Put kuda mi hotim zapisiat nashi vvedennie dannie
         // !!!! Ukazite zdes svoi put k failu
-        String filePath = "C:\\Users\\Администратор\\Desktop\\myFile\\File.txt";
+        String filePath = "D:\\myFile\\File.txt";
+
         //Sozdaem file po ukazanomu puti
         File file = new File(filePath);
+        System.out.println("Abs_path " + file.getAbsolutePath() + "  " + file.canWrite());
         try {
             //Klass kotori otvechiated za zapis v file
-            FileWriter bufferedWriter = new FileWriter(file);
+            FileWriter fileWriter = new FileWriter(file);
             //Zapis v file
-            bufferedWriter.append(input);
+            fileWriter.append(input);
             // Zakanchivaem zapis v file
-            bufferedWriter.close();
+            fileWriter.close();
         } catch (IOException e) {
             //Vivodim ssobshenie esli zapis ne udalas
-            System.out.println("Ne smog zapisiat v fail");
+            System.out.println("Ne smog zapisiat v fail " + e.toString());
         }
 
 
         // Vivod teksta s klaviaturi
         System.out.println(input);
+
+
+        //Sozdaiet massiv 10X10 zapolnenne 1
+        createArray();
+    }
+
+
+    public static void createArray() {
+        int[][] array = new int[10][10];
+        String filePath = "D:\\myFile\\File.txt";
+
+        //Sozdaem file po ukazanomu puti
+        File file = new File(filePath);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    array[i][j] = 1;
+                    fileWriter.append( String.valueOf(array[i][j]) + " ");
+                }
+                fileWriter.append("\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //arrayToFile(Arrays.toString(array));
     }
 
 }
