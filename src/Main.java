2bd2b9eef0fe32de8a573e821cb04d09c371fdc7
Murager-ПК\Murager <zmurager@gmail.com>
2017-02-01 19:59:54 +0600

    Added new line of code with comments.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -9,11 +13,30 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод текста
+        // Vvod teksta s klaviaturi
         String input = scanner.nextLine();
 
 
-        // Вывод текста
+
+        //Put kuda mi hotim zapisiat nashi vvedennie dannie
+        // !!!! Ukazite zdes svoi put k failu
+        String filePath = "C:\\Users\\Администратор\\Desktop\\myFile\\File.txt";
+        //Sozdaem file po ukazanomu puti
+        File file = new File(filePath);
+        try {
+            //Klass kotori otvechiated za zapis v file
+            FileWriter bufferedWriter = new FileWriter(file);
+            //Zapis v file
+            bufferedWriter.append(input);
+            // Zakanchivaem zapis v file
+            bufferedWriter.close();
+        } catch (IOException e) {
+            //Vivodim ssobshenie esli zapis ne udalas
+            System.out.println("Ne smog zapisiat v fail");
+        }
+
+
+        // Vivod teksta s klaviaturi
         System.out.println(input);
     }
 
