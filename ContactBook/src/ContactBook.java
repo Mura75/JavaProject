00a2c38f856b7fbe1ada35c79d05712f0aded1b7
Murager-ПК\Murager <zmurager@gmail.com>
2017-02-08 20:32:41 +0600

    Scanner is a parameter for createContact method.

--- ContactBook/src/ContactBook.java
+++ ContactBook/src/ContactBook.java
@@ -19,8 +19,7 @@ public class ContactBook {
         while (scanner.hasNext()) {
             String menuOption = scanner.nextLine();
             if (menuOption.equals("1")) {
-                createNewContact();
-                break;
+                createNewContact(scanner);
             }
             if (menuOption.equals("5")) {
                 System.out.println("Program stopped");
@@ -31,29 +30,44 @@ public class ContactBook {
     }
 
 
-    public static void createNewContact() {
+    public static void createNewContact(Scanner scanner) {
         System.out.println("Create new contact");
         Contact contact = new Contact();
-        Scanner scanner = new Scanner(System.in);
         System.out.println("a) Enter name");
         System.out.println("b) Enter surname");
         System.out.println("c) Enter phone");
-        System.out.println("e) Enter email");
-        System.out.println("d) exit");
+        System.out.println("d) Enter email");
+        System.out.println("e) exit");
         while (scanner.hasNext()) {
             String data = scanner.nextLine();
             if (data.equals("a")) {
+                System.out.println("Enter name:");
                 String name = scanner.nextLine();
                 contact.setName(name);
                 System.out.println("Contact name is: " + contact.getName());
             }
             else if (data.equals("b")) {
-                contact.setSurname(data);
+                String surname = scanner.nextLine();
+                contact.setSurname(surname);
                 System.out.println("Contact surname is: " + contact.getSurname());
-
             }
-            else if (data.equals("x")) {
-                contact.toString();
+            else if (data.equals("c")) {
+                String phone = scanner.nextLine();
+                contact.setPhone(phone);
+                System.out.println("Contact phone is: " + contact.getPhone());
+            }
+            else if (data.equals("d")) {
+                String email = scanner.nextLine();
+                contact.setEmail(email);
+                System.out.println("Contact email is: " + contact.getEmail());
+            }
+            else if (data.equals("e")) {
+                System.out.println("Contact created!" + "\n\n\n");
+                System.out.println("1) Create contact" + "\n" +
+                        "2) Change contact" + "\n" +
+                        "3) Delete contact" + "\n" +
+                        "4) Show contact list" + "\n" +
+                        "5) Exit");
                 break;
             }
         }
